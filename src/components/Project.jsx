e70e2ae272d083235a4ac6c3bd073e74c449de8c
abdulazeezcoding <abdulazizmohammed5103@gmail.com>
2024-05-02 04:22:12 +0000

    replace agro market with freshmarket

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import movieapp from "../assets/movie app.png";
-import agro from "../assets/agro .png";
+import freshmarket from "../assets/freshmarket.png";
 
 import abochi from "../assets/abochi-screen.png";
 
@@ -52,24 +52,24 @@ const Project = () => {
             </div>
             <div className="mb-4 lg:mb-0 p-4 lg:p-0 w-full md:w-4/7  rounded block border-4 border-gray-500">
               <img
-                src={agro}
+                src={freshmarket}
                 className="rounded-md object-cover w-full h-64"
               />
 
               <h1 className="my-5 ml-8 text-4xl font-bold leading-tight">
-                AgroMarket Place
+                  Fresh Market Dashboard
               </h1>
 
               <div className="flex justify-between pb-9">
                 <a
-                  href="https://ruby-alpha.github.io/ruby-agro-marketplace"
+                  href="https://github.com/abdulazeezcoding/FreshMarketFrontend"
                   target="_blank"
                   className="inline-block px-6 py-3 mt-2 rounded-md bg-gray-700 text-gray-100 ml-3 "
                 >
                   <FaGithub /> Repo
                 </a>
                 <a
-                  href="github/rofiatmuritala"
+                  href="https://fresh-market-frontend.vercel.app/"
                   className="inline-block px-6 py-3 mt-2 rounded-md bg-gray-700 text-gray-100 mr-3"
                 >
                   <FaExternalLinkAlt /> Live Demo
